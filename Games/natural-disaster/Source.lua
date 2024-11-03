a245afce769d2e14a22f540f2513a5c57b7b057d
Gatin <154676681+Gatin19@users.noreply.github.com>
2024-11-03 07:46:33 -0300

    Update Source.lua

--- Games/natural-disaster/Source.lua
+++ Games/natural-disaster/Source.lua
@@ -1,1 +1,11 @@
+local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 
+local Window = Fluent:CreateWindow({
+    Title = "Natural disaster",
+    SubTitle = "by gatin__",
+    TabWidth = 160,
+    Size = UDim2.fromOffset(580, 460),
+    Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
+    Theme = "Dark",
+    MinimizeKey = Enum.KeyCode.LeftControl -- Used when theres no MinimizeKeybind
+})
