8406da0a5e85e8633e2a3f26ababe00215e14641
Gatin <154676681+Gatin19@users.noreply.github.com>
2024-11-03 10:26:53 -0300

    Update Source.lua

--- Games/natural-disaster/Source.lua
+++ Games/natural-disaster/Source.lua
@@ -19,7 +19,7 @@ Toggle.TextColor3 = Color3.fromRGB(248, 248, 248)
 Toggle.TextSize = 28.000
 Toggle.Draggable = true
 Toggle.MouseButton1Click:connect(function()
-   game:GetService("VirtualInputManager"):SendKeyEvent(true, "F6" , false , game)
+   game:GetService("VirtualInputManager"):SendKeyEvent(true, "9" , false , game)
 end)
 
 local Corner = Instance.new("UICorner")
