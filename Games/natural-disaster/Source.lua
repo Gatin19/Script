20b483fef38decea61af8e050506167eda7456b5
Gatin <154676681+Gatin19@users.noreply.github.com>
2024-11-03 09:38:56 -0300

    Update Source.lua

--- Games/natural-disaster/Source.lua
+++ Games/natural-disaster/Source.lua
@@ -1,1 +1,1 @@
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/Gatin19/Script/refs/heads/main/Source.lua')))()
